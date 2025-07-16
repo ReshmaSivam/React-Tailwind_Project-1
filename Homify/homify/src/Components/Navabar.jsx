b0cc882725d829cd833d5943b9f3cc5a5d9e7087
ReshmaSivam <pmreshma.m@gmail.com>
2025-07-16 17:25:07 +0530

    created navabar for smaller screen

--- Homify/homify/src/Components/Navabar.jsx
+++ Homify/homify/src/Components/Navabar.jsx
@@ -13,7 +13,17 @@ const Navabar = () => {
             <a href="Header" className='cursor-pointer hover:text-gray-400'> Testimonials </a>
           </ul>
           <button className='hidden md:block bg-white px-8 py-2 rounded-full'> SignUp </button>
+          <img src={assets.menu_icon} className='md:hidden w-7 ' alt="" />
       </div> 
+      {/*---mobile-view---*/}
+      <div className='md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all'>
+        <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+           <a href="#Header" className='px-4 py-2 rounded full inline-block'> Home </a>
+           <a href="#About" className='px-4 py-2 rounded full inline-block'> About </a>
+           <a href="#Project" className='px-4 py-2 rounded full inline-block'> Project </a>
+           <a href="Testimonials" className='px-4 py-2 rounded full inline-block'> Testimonials </a> 
+        </ul>
+      </div>
     </div>
   )
 }
